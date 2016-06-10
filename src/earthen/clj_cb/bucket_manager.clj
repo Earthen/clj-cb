d6a0b9006eb93e38abf780fea905d09632565879
mbadia <mbadia@oneboxtm.com>
2016-06-10 14:42:14 +0200

    adding views

--- src/earthen/clj_cb/bucket_manager.clj
+++ src/earthen/clj_cb/bucket_manager.clj
@@ -1,9 +1,24 @@
 (ns earthen.clj-cb.bucket-manager
   (:refer-clojure :exclude [flush])
   (:require [earthen.clj-cb.utils :as u])
-  (:import [com.couchbase.client.java.bucket BucketManager]))
+  (:import [com.couchbase.client.java.bucket BucketManager]
+           [com.couchbase.client.java.view SpatialView DesignDocument]))
+
+(defn create-spatial-view
+  [name string]
+  (SpatialView/create name string))
+
+(defn create-design-document
+  "Name and list of views"
+  [name a-list]
+  (DesignDocument/create name a-list))
+
+(defn insert-design-document
+  [bucket-manager design]
+  (.insertDesignDocument bucket-manager design))
 
 (defn flush
+  "On 2.2.7 doesn't work properly"
   ([bucket-manager]
    (flush bucket-manager 60 :SECONDS))
   ([bucket-manager time time-type]
