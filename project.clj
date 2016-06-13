b10b4ad7cb8f17945a84b4f46940532787c1f171
Alfrheim <marc.badiac@gmail.com>
2016-06-14 00:46:51 +0200

    updating client

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.couchbase.client/java-client "2.2.1"]]
+                 [com.couchbase.client/java-client "2.2.7"]]
   :plugins [[lein-codox "0.9.4"]])
